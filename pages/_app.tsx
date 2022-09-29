e43835b38eb493de16865375656b5712ca176f42
Leo Lin <sayaww@gmail.com>
2022-09-29 12:14:12 +0800

    feat: added rainbow kit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,59 @@
-import type { AppProps } from "next/app";
-import { ThemeProvider } from "next-themes";
-import { darkTheme } from "../stitches.config";
+import '@rainbow-me/rainbowkit/styles.css';
+import type { AppProps } from 'next/app';
+import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import { ThemeProvider } from 'next-themes';
+import { darkTheme } from '../stitches.config';
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [
+    chain.mainnet,
+    chain.polygon,
+    chain.optimism,
+    chain.arbitrum,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
+      ? [chain.goerli, chain.kovan, chain.rinkeby, chain.ropsten]
+      : []),
+  ],
+  [
+    alchemyProvider({
+      // This is Alchemy's default API key.
+      // You can get your own at https://dashboard.alchemyapi.io
+      apiKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
+    }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'RainbowKit App',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      value={{
-        light: "light",
-        dark: darkTheme,
-      }}
-    >
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          value={{
+            light: 'light',
+            dark: darkTheme,
+          }}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import type { NextPage } from "next";
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
     <div>
       <h1>Main page</h1>
+      <ConnectButton />
     </div>
   );
 };
