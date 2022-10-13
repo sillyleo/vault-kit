bbb4439971e6925cc9030bddf407ab50ee517010
Leo Lin <sayaww@gmail.com>
2022-10-13 14:33:35 +0800

    button style

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,13 +1,51 @@
 import { styled } from "@stitches/react";
 
 export const Button = styled("button", {
-  backgroundColor: "$mint5",
-  fontSize: 15,
+  // base style
+  fontFamily: "$body",
   fontWeight: "bold",
-  padding: "10px 20px",
-  color: "$mint11",
-  borderColor: "$mint6",
-  display: "flex",
+  letterSpacing: "$tight",
+  display: "inline-flex",
   alignItems: "center",
   gap: "0.5em",
+  backgroundColor: "$blue9",
+  borderColor: "$blue9",
+  color: "$blue2",
+  borderWidth: 0,
+  borderRadius: "$full",
+  cursor: "pointer",
+  transition: "transform 0.2s linear",
+
+  "&:hover": {
+    backgroundColor: "$blue10",
+    transform: "scale(1.02)",
+  },
+
+  "&:active": {
+    backgroundColor: "$blue10",
+    transform: "scale(0.98)",
+  },
+
+  // variants (use as props)
+
+  variants: {
+    color: {},
+    size: {
+      "1": {
+        fontSize: "$1",
+        padding: "$1 $2",
+      },
+      "2": {
+        fontSize: "$3",
+        padding: "$2 $3",
+      },
+      "3": {
+        fontSize: "$4",
+        padding: "$2 $4",
+      },
+    },
+  },
+  defaultVariants: {
+    size: "2",
+  },
 });
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,24 @@
-import '@rainbow-me/rainbowkit/styles.css';
-import type { AppProps } from 'next/app';
+import "@rainbow-me/rainbowkit/styles.css";
+import type { AppProps } from "next/app";
 import {
   RainbowKitProvider,
   getDefaultWallets,
   darkTheme as walletDarkTheme,
+  lightTheme as walletLightTheme,
   DisclaimerComponent,
-} from '@rainbow-me/rainbowkit';
-import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
-import { ThemeProvider } from 'next-themes';
-import { darkTheme } from '../stitches.config';
+} from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { ThemeProvider } from "next-themes";
+import { darkTheme } from "../stitches.config";
 import "@fontsource/inter/";
-
 
 const { chains, provider, webSocketProvider } = configureChains(
   [
     chain.mainnet,
     chain.optimism,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
       ? [chain.goerli]
       : []),
   ],
@@ -26,14 +26,14 @@ const { chains, provider, webSocketProvider } = configureChains(
     alchemyProvider({
       // This is Alchemy's default API key.
       // You can get your own at https://dashboard.alchemyapi.io
-      apiKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
+      apiKey: "_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC",
     }),
     publicProvider(),
   ]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
+  appName: "RainbowKit App",
   chains,
 });
 
@@ -45,9 +45,9 @@ const wagmiClient = createClient({
 });
 const Disclaimer: DisclaimerComponent = ({ Text, Link }) => (
   <Text>
-    By connecting your wallet, you agree to the{' '}
+    By connecting your wallet, you agree to the{" "}
     <Link href="https://termsofservice.xyz">Terms of Service</Link> and
-    acknowledge you have read and understand the protocol{' '}
+    acknowledge you have read and understand the protocol{" "}
     <Link href="https://disclaimer.xyz">Disclaimer</Link>
   </Text>
 );
@@ -56,20 +56,26 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider
+        theme={{
+          lightMode: walletLightTheme({ fontStack: "system" }),
+          darkMode: walletDarkTheme({ fontStack: "system" }),
+        }}
         appInfo={{
-          appName: 'Rainbowkit Demo',
-          learnMoreUrl: 'https://perp.com',
+          appName: "Rainbowkit Demo",
+          learnMoreUrl: "https://perp.com",
           disclaimer: Disclaimer,
         }}
         showRecentTransactions={true}
-        chains={chains}>
+        chains={chains}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           value={{
-            light: 'light',
+            light: "light",
             dark: darkTheme,
-          }}>
+          }}
+        >
           <Component {...pageProps} />
         </ThemeProvider>
       </RainbowKitProvider>
